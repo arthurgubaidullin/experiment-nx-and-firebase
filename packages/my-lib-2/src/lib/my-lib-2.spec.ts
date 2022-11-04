523ca0a57e282d2663c50b516e74ec80f1041ced
Artur Gubaidullin <lapteuh@gmail.com>
2022-11-04 14:09:59 +0400

    feat: add libs

---
+++ packages/my-lib-2/src/lib/my-lib-2.spec.ts
@@ -0,0 +1,7 @@
+import { myLib2 } from './my-lib-2';
+
+describe('myLib2', () => {
+  it('should work', () => {
+    expect(myLib2()).toEqual('my-lib:my-lib-2');
+  });
+});
---
+++ packages/my-lib-2/src/lib/my-lib-2.ts
@@ -0,0 +1,5 @@
+import { myLib } from '@experiment-nx-and-firebase/my-lib';
+
+export function myLib2(): string {
+  return `${myLib()}:my-lib-2`;
+}
